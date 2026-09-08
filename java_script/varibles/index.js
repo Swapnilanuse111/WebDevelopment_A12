4ca883f3c025f823424653f33ec6082c21536162
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-09-08 20:15:21 +0530

    Day 42:Start JavaScript Learing From Basic

---
+++ java_script/varibles/index.js
@@ -0,0 +1,41 @@
+
+// // var a=10;
+// // console.log(a);
+// // var b;
+// // b=10;
+
+
+// // var b;
+// // b=20;
+
+// const a;
+// a=10;
+// console.log(a);
+// // a=30;
+// // print(a)
+
+
+// // let a=10;
+// // console.log(a)
+
+// // a=30;
+// // console.log(a)
+
+// console.log(a);
+// var a=10;
+
+
+
+// console.log(a);
+// const a=10;
+
+// sources-->left Side File Is--->clicked-->our Code Is There-->see number and click on that line break Point -->Do Refreshh---see Our console--then we exceute line by line->Click On Globle-->  
+
+// console.log(a);
+// var a=10;
+// console.log(a)
+
+
+console.log(a);
+let a=10;
+console.log(a);
